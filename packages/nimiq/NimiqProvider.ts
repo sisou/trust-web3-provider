97448fab7610a69aee33dfff00bccfac39c28cd8
Sören <hello@soerenschwert.de>
2026-01-15 15:52:16 +0100

    Add basic methods for NimiqProvider
    Using method names, parameters and result types from Nimiq's RPC interface.

--- packages/nimiq/NimiqProvider.ts
+++ packages/nimiq/NimiqProvider.ts
@@ -1,8 +1,41 @@
 
 
-import { BaseProvider } from '@trustwallet/web3-provider-core';
+import { BaseProvider, type IRequestArguments } from '@trustwallet/web3-provider-core';
 import type INimiqProvider from './types/NimiqProvider';
 import type { INimiqProviderConfig } from './types/NimiqProvider';
+
+export interface SignatureResult {
+  publicKey: string,
+  signature: string,
+}
+
+export interface TransactionInfo {
+  hash: string,
+  blockNumber: number,
+  timestamp: number,
+  confirmations: number,
+  size: number,
+  relatedAddresses: string[],
+  from: string,
+  fromType: number,
+  to: string,
+  toType: number,
+  value: number,
+  fee: number,
+  senderData: string,
+  recipientData: string,
+  flags: number,
+  validityStartHeight: number,
+  proof: string,
+  networkId: number,
+}
+
+export interface ErrorResponse {
+  error: {
+    type: string,
+    message: string,
+  }
+}
 
 export class NimiqProvider
   extends BaseProvider
@@ -19,20 +52,68 @@ export class NimiqProvider
     return NimiqProvider.NETWORK;
   }
 
-  async request(args: { method: string; params?: any[] }): Promise<any> {
-    // Normalize method names (these are the names sent to the native app)
-    switch (args.method) {
-      case 'nim_requestAccounts':
-        args.method = 'requestAccounts';
-        break;
-      case 'nim_isConsensusEstablished':
-        args.method = 'isConsensusEstablished';
-        break;
-      // Add more method normalizations if needed
-      default:
-        throw new Error(`Unsupported method: ${args.method}`);
-    }
+  listAccounts(): Promise<string[] | ErrorResponse> {
+    return this.#internalRequest<string[]>({ method: 'listAccounts' });
+  }
 
-    return super.request(args);
+  sign(message: string | { message: string, isHex?: boolean }): Promise<SignatureResult | ErrorResponse> {
+    return this.#internalRequest<SignatureResult>({
+      method: 'sign',
+      params: typeof message === 'string' ? { message } : message,
+    });
+  }
+
+  isConsensusEstablished(): Promise<boolean> {
+    return this.#internalRequest<boolean>({ method: 'isConsensusEstablished' });
+  }
+
+  getBlockNumber(): Promise<number> {
+    return this.#internalRequest<number>({ method: 'getBlockNumber' });
+  }
+
+  /**
+   * Sign and send a basic transaction from the wallet
+   * @param tx Transaction parameters: recipient, value, fee (optional), validityStartHeight (optional)
+   * @returns The serialized transaction
+   */
+  sendBasicTransaction(tx: {
+    recipient: string,
+    value: number,
+    fee?: number,
+    validityStartHeight?: number,
+  }): Promise<string | ErrorResponse> {
+    return this.#internalRequest<string>({
+      method: 'sendBasicTransaction',
+      params: tx,
+    });
+  }
+
+  /**
+   * Sign and send a basic transaction from the wallet
+   * @param tx Transaction parameters: recipient, value, fee (optional), validityStartHeight (optional)
+   * @returns The serialized transaction
+   */
+  sendBasicTransactionWithData(tx: {
+    recipient: string,
+    value: number,
+    fee?: number,
+    data: string,
+    validityStartHeight?: number,
+  }): Promise<string | ErrorResponse> {
+    return this.#internalRequest<string>({
+      method: 'sendBasicTransactionWithData',
+      params: tx,
+    });
+  }
+
+  // TODO: Add other transaction creation types
+
+  /**
+   * Call request handler directly
+   * @param args
+   * @returns
+   */
+  #internalRequest<T>(args: IRequestArguments): Promise<T> {
+    return super.request<T>(args);
   }
 }
